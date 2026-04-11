cad785ad74e0eca6398db93967bd44bd472a5208
snail5039-code <snail5039@gmail.com>
2026-04-12 02:28:43 +0900

    Fix studentDto getters and constructor

--- backend/src/main/java/com/jetrace/backend/studentDto/AiResponseDto.java
+++ backend/src/main/java/com/jetrace/backend/studentDto/AiResponseDto.java
@@ -1,10 +1,41 @@
 package com.jetrace.backend.studentDto;
 
-import lombok.Data;
-
-@Data
 public class AiResponseDto {
+
+    private String answer;
     private boolean relevant;
     private int score;
-    private String answer;
+
+    public AiResponseDto() {
+    }
+
+    public AiResponseDto(String answer, boolean relevant, int score) {
+        this.answer = answer;
+        this.relevant = relevant;
+        this.score = score;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isRelevant() {
+        return relevant;
+    }
+
+    public void setRelevant(boolean relevant) {
+        this.relevant = relevant;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
--- backend/src/main/java/com/jetrace/backend/studentDto/ChatResponseDto.java
+++ backend/src/main/java/com/jetrace/backend/studentDto/ChatResponseDto.java
@@ -1,13 +1,51 @@
 package com.jetrace.backend.studentDto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class ChatResponseDto {
+
     private boolean relevant;
     private int score;
     private String answer;
-    private String status;
+    private String message;
+
+    public ChatResponseDto() {
+    }
+
+    public ChatResponseDto(boolean relevant, int score, String answer, String message) {
+        this.relevant = relevant;
+        this.score = score;
+        this.answer = answer;
+        this.message = message;
+    }
+
+    public boolean isRelevant() {
+        return relevant;
+    }
+
+    public void setRelevant(boolean relevant) {
+        this.relevant = relevant;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
